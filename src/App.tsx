ad71a3aae45ed0b23e9fe64498a24695ffdaae89
suhangowda96 <suhangowda46@gmail.com>
2024-11-30 20:55:43 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -149,7 +149,7 @@ function App() {
               </motion.div>
               <motion.a 
                 whileHover={{ scale: 1.05 }}
-                href="https://drive.google.com/file/d/1KbVA9TRw3wwqYmkwL9VyjFmyGkdWASao/view?usp=sharing"
+                href="https://drive.google.com/file/d/1AlRAJl_ThT6cFVhgVQjLB3otnz7UzI9N/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-flex items-center bg-gradient-to-r from-purple-600 to-pink-600 text-white px-8 py-3 rounded-full hover:from-purple-700 hover:to-pink-700 transition-all shadow-lg"
