7341e2038de032ab7729e7609207924bb466dacc
suhangowda96 <suhangowda46@gmail.com>
2025-06-03 10:10:30 +0530

    ExperienceSection.tsx

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { ExperienceCard } from './ExperienceCard';
 
 export function ExperienceSection() {
-  const Experience = [
+  const experience = [
     {
       company: "Unified Mentors",
       role: "Data Science Intern",
